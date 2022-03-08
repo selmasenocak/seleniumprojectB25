f764653956de91bb230cf74520cda79fb38200fb
selmasenocak <sbsenocak@gmail.com>
2022-03-08 15:36:33 -0500

    new T3_Actions_Hover.java

---
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_Hover.java
@@ -0,0 +1,26 @@
+package com.cydeo.tests.day10_upload_actions_jsexecutor;
+
+import org.testng.annotations.Test;
+
+public class T3_Actions_Hover {
+
+    @Test
+    public void hovering_test() {
+
+
+//    TC #3: Hover Test
+//1. Go to https://practice.cydeo.com/hovers
+//            2. Hover over to first image
+//3. Assert:
+//    a. “name: user1” is displayed
+//    b. “view profile” is displayed
+//4. Hover over to second image
+//5. Assert:
+//    a. “name: user2” is displayed
+//    b. “view profile” is displayed
+//6. Hover over to third image
+//7. Confirm:
+//    a. “name: user3” is displayed
+//    b. “view profile” is displayed
+    }
+}
